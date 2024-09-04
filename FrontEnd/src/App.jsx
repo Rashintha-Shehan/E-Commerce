57c2d95b0d532865f31acf4174a178e831593a37
Rashintha shehan <rashinthas427@gmail.com>
2024-09-04 22:51:53 +0530

    NavBar

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
 import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlacedOrder from './pages/PlacedOrder'
+import Orders from './pages/Orders'
+import NavBar from './Component/NavBar'
+
+
+
 const App =()=>{
   return(
     <div className='px-4 sm:px[5vw] md:px[7vw] lg:px-[9vw]'>
