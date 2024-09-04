be28bd017071d30fb7adb286c03def3cfba298ab
Rashintha shehan <rashinthas427@gmail.com>
2024-09-04 17:51:17 +0530

    CreatePages& Routing

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,11 +4,17 @@ import Collection from './pages/Collection'
 const App =()=>{
   return(
     <div className='px-4 sm:px[5vw] md:px[7vw] lg:px-[9vw]'>
-
+<NavBar/>
 <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/Collection' element={<Collection/>}/>
-
+  <Route path='/About' element={<About/>}/>
+  <Route path='/Contact' element={<Contact/>}/>
+  <Route path='/Product/:ProductId' element={<Product/>}/>
+  <Route path='/Cart' element={<Cart/>}/>
+  <Route path='/Login' element={<Login/>}/>
+  <Route path='/PlacedOrder' element={<PlacedOrder/>}/>
+  <Route path='/Orders' element={<Orders/>}/>
 </Routes>
     </div>
   )
